1d04c3b69ab20ead7abd6a29a568c2b8c604811a
Harrison Goosen <harrison.goosen@okstate.edu>
2021-03-20 15:15:31 -0500

    Run as Root Patch
    patch to be able to run as root

--- src/main.cpp
+++ src/main.cpp
@@ -32,25 +32,10 @@
 #include <QDBusConnectionInterface>
 #include <QSessionManager>
 
-#ifndef Q_OS_WIN
-#include <unistd.h>
-#endif
 #include <iostream>
 
 extern "C" Q_DECL_EXPORT int kdemain(int argc, char **argv)
 {
-#ifndef Q_OS_WIN
-    // Prohibit using sudo or kdesu (but allow using the root user directly)
-    if (getuid() == 0) {
-        if (!qEnvironmentVariableIsEmpty("SUDO_USER")) {
-            std::cout << "Executing Dolphin with sudo is not possible due to unfixable security vulnerabilities." << std::endl;
-            return EXIT_FAILURE;
-        } else if (!qEnvironmentVariableIsEmpty("KDESU_USER")) {
-            std::cout << "Executing Dolphin with kdesu is not possible due to unfixable security vulnerabilities." << std::endl;
-            return EXIT_FAILURE;
-        }
-    }
-#endif
 
     /**
      * enable high dpi support
